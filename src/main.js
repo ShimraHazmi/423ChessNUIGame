1478e743359c72ff7cc2139e630ce7c811b8d91c
Brian Daza <bdaza3@uic.edu>
2026-02-17 12:51:27 -0600

    implement chessboard onto frontend
    downloaded and added chessboard.js files

--- src/main.js
+++ src/main.js
@@ -3,3 +3,10 @@ import { startListening } from "./speech.js";
 document.getElementById("start").onclick = () => {
   startListening();
 };
+
+var board1 = Chessboard('board1', 'start');
+
+var board1 = Chessboard('board1', {
+  position: 'start',
+  pieceTheme: './src/chessboardjs-1.0.0/img/chesspieces/wikipedia/{piece}.png'
+});
